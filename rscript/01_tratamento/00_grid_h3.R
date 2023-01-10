0dbb4da6944640e716376578cda3fb91ff791667
Beatriz Moura <biamouras@gmail.com>
2023-01-10 11:08:25 -0300

    update h3jsr functions

--- rscript/01_tratamento/00_grid_h3.R
+++ rscript/01_tratamento/00_grid_h3.R
@@ -6,6 +6,8 @@
 library(tidyverse)
 library(h3jsr)
 
+grid_resolution <- 9
+
 # Files ----
 msp <- sf::st_read('data/shp/MSP.shp') %>% 
     sf::st_transform(crs = 4326) # WGS84 degrees
@@ -14,33 +16,27 @@ msp <- sf::st_read('data/shp/MSP.shp') %>%
 
 # get the ids
 # get the unique h3 ids of the hexagons intersecting your polygon at a given resolution
-hex_9 <- h3jsr::polyfill(msp, res = 9, simple = FALSE)
+hex <- polygon_to_cells(msp, res = grid_resolution, simple = FALSE)
 
 # pass the h3 ids to return the hexagonal grid
-hex_9 <- unlist(hex_9$h3_polyfillers) %>% 
-    h3jsr::h3_to_polygon(simple = FALSE)
+hex <- unlist(hex$h3_addresses) %>% 
+  cell_to_polygon(simple = FALSE)
 
-grid <- hex_9 %>% 
-    dplyr::mutate(id = h3_address) %>% 
-    dplyr::select(id)
+grid <- hex %>% 
+    mutate(id = h3_address) %>% 
+    select(id)
 
 # Saving ----
 # as shapefile
 
-grid <- sf::as_Spatial(grid)
-rgdal::writeOGR(grid, driver='ESRI Shapefile', dsn='data/shp/grid_hex_MSP.shp', layer='grid')
-
-g <- sf::read_sf('data/shp/grid_hex_MSP.shp')
+sf::write_sf(grid, paste0('data/shp/grid_hex_MSP_', grid_resolution, '.shp'))
 
 # Points coord ----
-# After selecting only the cells that will show on the map
-grid_clean <- sf::st_read('data/shp/grid_hex_MSP_clean.shp')
 
-points <- grid_clean %>% 
-    dplyr::rowwise() %>% 
-    dplyr::mutate(coords = paste0(sf::st_coordinates(h3jsr::h3_to_point(id)), collapse=',')) %>% 
-    tidyr::separate(col = coords, into = c('X', 'Y'), sep=',') %>% 
-    sf::st_as_sf() %>% 
-    sf::st_drop_geometry()
+points <- grid %>% 
+  rowwise() %>% 
+  mutate(coords = paste0(sf::st_coordinates(cell_to_point(id)), collapse=',')) %>% 
+  separate(col = coords, into = c('X', 'Y'), sep=',') %>% 
+  sf::st_drop_geometry()
 
-write.csv(points, 'otp/points/points_hex_clean.csv', row.names = F)
+write_csv(points, paste0('data/shp/grid_hex_MSP_', grid_resolution, '.csv'))
